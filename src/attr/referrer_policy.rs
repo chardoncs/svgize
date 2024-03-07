88e3ecd18b7443edc246d7eebb74c845820b1aa0
chardon55 <charlesdong_2000@outlook.com>
2024-03-07 17:34:50 +0800

    Add conversion to referrer policy

--- src/attr/referrer_policy.rs
+++ src/attr/referrer_policy.rs
@@ -17,9 +17,31 @@ impl FromStr for ReferrerPolicy {
     type Err = Error;
 
     fn from_str(s:&str) -> Result<Self, Self::Err> {
-        todo!();
-        match s {
-            _ => Err(Error::NoOptionFound),
-        }
+        Ok(match s {
+            "no-referrer" => Self::NoReferrer,
+            "no-referrer-when-downgrade" => Self::NoReferrerWhenDowngrade,
+            "origin" => Self::Origin,
+            "origin-when-cross-origin" => Self::OriginWhenCrossOrigin,
+            "same-origin" => Self::SameOrigin,
+            "strict-origin" => Self::StrictOrigin,
+            "strict-origin-when-cross-origin" => Self::StrictOriginWhenCrossOrigin,
+            "unsafe-url" => Self::UnsafeUrl,
+            _ => Err(Error::NoOptionFound)?,
+        })
+    }
+}
+
+impl ToString for ReferrerPolicy {
+    fn to_string(&self) -> String {
+        match self {
+            Self::NoReferrer => "no-referrer",
+            Self::NoReferrerWhenDowngrade => "no-referrer-when-downgrade",
+            Self::Origin => "origin",
+            Self::OriginWhenCrossOrigin => "origin-when-cross-origin",
+            Self::SameOrigin => "same-origin",
+            Self::StrictOrigin => "strict-origin",
+            Self::StrictOriginWhenCrossOrigin => "strict-origin-when-cross-origin",
+            Self::UnsafeUrl => "unsafe-url",
+        }.to_string()
     }
 }
